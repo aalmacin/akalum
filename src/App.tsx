cc71ee9ca4f89fedae939d13e4ca41ab432459a0
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2021-03-06 16:12:24 -0500

    Add styling for readme

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,41 @@ const AppDiv = styled.div`
   font-size: 3rem;
 `;
 
+const MarkdownStyles = styled.div`
+  font-size: 2rem;
+  p,
+  code {
+    font-size: 2rem;
+  }
+
+  h1 {
+    margin: 2.5rem 0;
+  }
+
+  h2 {
+    margin: 1rem 0;
+  }
+
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0.5rem 0;
+  }
+
+  li {
+    list-style-type: disc;
+    list-style-position: inside;
+    margin-top: 0.5rem;
+  }
+`;
+
 export const App: React.FC = () => (
   <>
     <Global styles={globalStyles} />
     <AppDiv>Welcome to Akalum!</AppDiv>
-    <ReactMarkdown children={`${MainReadme}`} />
+    <MarkdownStyles>
+      <ReactMarkdown children={`${MainReadme}`} />
+    </MarkdownStyles>
   </>
 );
